c797e6217373fd8374e9f20904af22cdde79ee0f
Danny Del Pezo <ninedelpezo@gmail.com>
2016-03-20 15:49:58 -0500

    Corregido errores en el script linea  1-4y5

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-Console.log("inicio de programa");
-alert("Prueba de errores funcionando")
-Document.write("<h1>Hola Java Script<p>");
-console.log("Fin de programa");
+console.log("inicio de programa");
+alert("Prueba de errores funcionando");
+document.write("<h1>Hola Java Script<p>");
 alert(Hola Java Script");
+console.log("Fin de programa");
