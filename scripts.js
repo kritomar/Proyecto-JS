3c99ef3587707ae65bc16fb29ceed956121b190d
karitomar <krito3791@gmail.com>
2016-03-20 17:01:27 -0500

    metodos de string

--- scripts.js
+++ scripts.js
@@ -1,5 +1,24 @@
-Console.log("inicio de programa");
-alert("Prueba de errores funcionando")
-Document.write("<h1>Hola Java Script<p>");
+/*console.log("inicio de programa");
+alert("Hola Krito =)");
+document.write("<h1>Hola Java Script<p>");
+alert("Hola Java Script");
 console.log("Fin de programa");
-alert(Hola Java Script");
+*/
+console.log("inicio de programa");
+/* */
+/*var equipo = prompt("Cual es tu equipo de futbol favorito");
+document.write("<h1> El nombre de su equipo favorito es: " + equipo +"</p>");*/
+/*console.log ("El equipo es: " + equipo );*/
+
+/*concatenar string*/
+/*
+var clave = prompt("Hola usuario, Ingrese su clave: ");
+console.log ("la longitud de la clave es:" + clave.length);
+*/
+
+var clave = prompt("Hola usuario, Ingrese su clave en minuscula: ");
+console.log ("la longitud de la clave es:" + clave.length);
+console.log ("TA-DA!!!!Su clave es:" + clave.toUpperCase());
+console.log ("El valor original de la clave es:" + clave);
+console.log("Fin de programa");
+
