18abb6386046611f669a9fa4e6a6370b65380e1f
karitomar <krito3791@gmail.com>
2016-03-20 17:37:50 -0500

    cuenta cuentos

---
+++ cuentos.js
@@ -0,0 +1,13 @@
+console.log("inicio de programa");
+var sustantivo = prompt("Ingresa un sustantivo: ");
+var adjetivo = prompt("Ingresa adjetivo: ");
+var verbo = prompt("Ingresa un verbo: ");
+document.write("<h1>El cuentacuentos dice: </p>");
+document.write("<p>El "+ verbo+" "+adjetivo+" es "+sustantivo+"</p>");
+document.write("<p>"+ sustantivo+" es el "+verbo+" "+adjetivo+" del Ecuador</p>");
+document.write("<p>"+verbo +" "+adjetivo+" que "+ sustantivo+" no hay carajo</p>");
+/*
+console.log ("la longitud de la clave es:" + clave.length);
+console.log ("TA-DA!!!!Su clave es:" + clave.toUpperCase());
+console.log ("El valor original de la clave es:" + clave);*/
+console.log("Fin de programa");
--- scripts.js
+++ scripts.js
@@ -15,10 +15,11 @@ document.write("<h1> El nombre de su equipo favorito es: " + equipo +"</p>");*/
 var clave = prompt("Hola usuario, Ingrese su clave: ");
 console.log ("la longitud de la clave es:" + clave.length);
 */
-
+/*
 var clave = prompt("Hola usuario, Ingrese su clave en minuscula: ");
 console.log ("la longitud de la clave es:" + clave.length);
 console.log ("TA-DA!!!!Su clave es:" + clave.toUpperCase());
 console.log ("El valor original de la clave es:" + clave);
 console.log("Fin de programa");
 
+*/
