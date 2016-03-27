5f0ea047a2d8604931c6074eacbdfd6824975516
Flor Vera O <ninedelpezo@gmail.com>
2016-03-27 09:26:55 -0500

    var tipo string y parseInt

---
+++ calculadora.js
@@ -0,0 +1,6 @@
+var segundosPorMinuto = 60;
+var minutosPorHora= 60;
+var horasPorDia=24;
+
+var segundosPorDia=segundosPorMinuto*minutosPorHora*horasPorDia;
+document.write("<h1>Calculadora del Tiempo</h1><p>Hay"+segundosPorDia+"en un dia</p>")
---
+++ casting.js
@@ -0,0 +1,7 @@
+
+console.log("inicio de programa");
+var peliculasVistas=prompt("Cuantas peliculas has visto este mes");
+var seriesVistas=prompt("Cuantas series has visto este mes?");
+var tiempoPantalla = parseInt(peliculasVistas) + parseInt(seriesVistas);
+alert('Wow! Has visto ' + tiempoPantalla + " veces minimo la pantalla.");
+console.log("Fin de programa");
