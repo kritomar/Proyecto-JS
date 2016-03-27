c8eff9810a4026e6511913d4bc54a1dbf0da42e9
Flor Vera O <ninedelpezo@gmail.com>
2016-03-27 15:49:21 -0500

    Funciones y uso de estructura while loop

--- funciones.js
+++ funciones.js
@@ -1,9 +1,93 @@
+
+console.log("inicio de programa");
+/*programa que nos permite almacenar valores enteros*/
+/*
 function tiraDados()
 {
 	var dado = Math.floor (Math.random()*6)+1;
-	alert (dado);
+ return dado;
 }
 alert("Tiramos 3 dados:");
-tiraDados();
-tiraDados();
-tiraDados();
+alert("El dado dice:"+tiraDados());
+var multiplo= 2 * tiraDados();
+alert("el multiplo de bonus es:"+ multiplo);
+*/
+
+//NUMERO AL AZAR
+/*
+function numeroAlAzarHastaLimite (limite)
+{
+	var num= Math.floor(Math.random()*limite)+1;
+	return num;
+}
+var numAzar= numeroAlAzarHastaLimite(100);
+alert(numAzar);
+console.log("Fin de programa");
+*/
+//FUNCION LLAMADA MAX QUE RETORNE VALOR MAXIMO DE DOS NUMEROS
+
+/*function max(num1, num2)
+{
+	if(num1>num2)
+	{
+		return num1;
+	}
+	else
+	{
+		return num2;
+	}
+}*/
+
+//funcion saludo
+/*
+function saludo()
+{
+	var mensaje="Hola a todos";
+	alert(mensaje);
+}
+var mensaje= "Chao";
+saludo();
+alert(mensaje);
+saludo();
+
+*/
+/*var num =9;
+if(isNa(num)N)
+{
+
+	throw new Error("No es un numero valido");
+}else {
+	num=num*0.5;
+	//alert(num);
+}
+console.log("fin de programa");
+*/
+
+//DESAFIO
+
+function numeroAlAzarEntreLimites (limiteInferior, limiteSuperior)
+{
+
+	if(isNaN(limiteInferior) || isNaN(limiteSuperior))
+		{
+			throw new Error("No es un numero valido");
+		}
+
+		var num= Math.round(Math.random()*(limiteSuperior-limiteInferior))+limiteInferior;
+		return num;
+}
+
+var limiteSuperior=prompt("Ingrese limite superior");
+var limiteInferior=prompt("Ingrese limite inferior");
+
+alert ("Su numero al azar es:"+ numeroAlAzarEntreLimites(parseInt(limiteInferior), parseInt(limiteSuperior)));
+
+// WHILE LOOP
+var contador =0;
+while(contador<10)
+{
+
+	var randomico = numeroAlAzarEntreLimites(parseInt(limiteInferior), parseInt(limiteSuperior));
+	document.write(randomico + "");
+	contador++;
+}
